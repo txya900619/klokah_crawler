d22dbfeb0b624bd9536d3bc3fc8117a9874da9d4
txya900619 <wayne900619@gmail.com>
2024-07-17 10:06:22 +0800

    feat: 🎸 add interact and lima to crawl_all.sh

--- crawl_all.sh
+++ crawl_all.sh
@@ -1,4 +1,4 @@
-for spider in readingtext livetutorial twelve twelve_word readnews sp_junior sp_senior ps_practice con_practice con_practice_word rd_practice rd_practice_word cu_practice cu_practice_word dialogue dialogue_word essay essay_word pbc mode
+for spider in readingtext livetutorial twelve twelve_word readnews sp_junior sp_senior ps_practice con_practice con_practice_word rd_practice rd_practice_word cu_practice cu_practice_word dialogue dialogue_word essay essay_word pbc mode interact lima
 do
     scrapy crawl $spider -O results/$spider.json:jsonlines --logfile logs/$spider.log
 done
